4803e267306be6a528e56458a8c68406c1381c5c
Shawn-Fries <samfoley275@gmail.com>
2022-02-11 15:20:48 -0500

    clock done

--- frontend/clock.jsx
+++ frontend/clock.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 
 class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date()
+        };
+        this.tick = this.tick.bind(this);
+        this.interval = 0;
+    }
 
+    tick() {
+        //debugger;
+        this.setState({time: new Date()})
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(this.tick, 1000);
+    }
 
     render() {
+        console.log(this.state.time)
         return (
-            <h1>Clock</h1>
+           
+            <div>
+                <h1>Clock</h1>
+                <div>
+                    {this.state.time.getHours()}:{this.state.time.getMinutes()}:{this.state.time.getSeconds()}
+                </div>
+
+
+            </div>
+            
         )
     }
 }
